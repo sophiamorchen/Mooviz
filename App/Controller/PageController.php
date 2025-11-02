93a15ea720d8a22efc78ba12441430c67d938326
Sophia M <sophia.moerchen@gmail.com>
2025-11-02 18:03:23 +0100

    mise en place de l'ossature MVC

---
+++ App/Controller/PageController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+class PageController extends Controller
+{
+    public function route(): void
+    {
+        try {
+            if (isset($_GET['action'])) {
+                switch ($_GET['action']) {
+                    case 'about':
+                        //On appelle la méthode about()
+                        $this->about();
+                        break;
+                    case 'home':
+                        //charger controleur home
+                        $this->home();
+                        break;
+                    default:
+                        throw new \Exception("Cette action n'existe pas : " . $_GET['action']);
+                        break;
+                }
+            } else {
+                throw new \Exception("Aucune action détectée");
+            }
+        } catch (\Exception $e) {
+            $this->render('errors/default', [
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    /*
+    Exemple d'appel depuis l'url
+        ?controller=page&action=about
+    */
+    protected function about()
+    {
+        /* On passe en premier paramètre la page à charger
+            et en deuxième un tableau associatif de paramètres
+        */
+        $this->render('page/about', [
+            'test' => 'abc',
+            'test2' => 'abc2',
+        ]);
+    }
+
+    /*
+    Exemple d'appel depuis l'url
+        ?controller=page&action=home
+    */
+    protected function home()
+    {
+        $this->render('page/home', []);
+    }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+define('_ROOTPATH_', __DIR__);
+
+spl_autoload_register();
+
+use App\Controller\Controller;
+
+$controller = new Controller();
+$controller->route();
