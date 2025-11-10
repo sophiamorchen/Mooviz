d245d023dd524b5248f729bc054509688b33039e
Sophia M <sophia.moerchen@gmail.com>
2025-11-10 16:34:37 +0100

    Add createAndHydrate - table User - user login - config.php define - userRepository -   errors on auth

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define("_BOOKS_IMAGES_FOLDER_", "/uploads/books/");
-define("_ASSETS_IMAGES_FOLDER_", "/assets/images/");
-define("_HOME_BOOK_LIMIT_", 3);
-define("_ITEM_PER_PAGE_", 9);
+define("ROLE_USER", "user");
+define("ROLE_ADMIN", "admin");
+define("MOVIES_IMAGES_FOLDER", "/uploads/movies/");
+define("ASSETS_IMAGES_FOLDER", "/assets/images/");
